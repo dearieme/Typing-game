3d871fae9f92f99fee1ef24922a3998497f5f7f5
Oscar <oscarthomson2004@gmail.com>
2021-08-10 17:43:11 +0100

    attempted to show random line from file

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import time
+import random
 
-def readNextXLines(fileName):
-    f = open(fileName, r)
-    lines = f.readLines()
+def readLines(fileName):
+    f = open(fileName, "r")
+    lines = f.readlines()
     f.close()
     return lines
 
+def isNewLine(string):
+    if string == "\n":
+        return True
+    else:
+        return False
+
+lines = readLines("thingsToType.txt")
+#filter newline function
+line = lines[random.randint(0, len(lines))]
+
+print(lines)
+print(line)
