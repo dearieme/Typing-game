68f405de0710abaa0754bf53d0485db44368e0e0
Oscar <oscarthomson2004@gmail.com>
2021-08-10 18:23:12 +0100

    Added ability for user to enter string

--- main.py
+++ main.py
@@ -8,14 +8,44 @@ def readLines(fileName):
     return lines
 
 def isNewLine(string):
-    if string == "\n":
+    if string != "\n":
         return True
     else:
         return False
 
-lines = readLines("thingsToType.txt")
-#filter newline function
-line = lines[random.randint(0, len(lines))]
+def getLine():
+    lines = readLines("thingsToType.txt")
+    lines = list(filter(isNewLine, lines))
+    line = lines[random.randint(0, len(lines)-1)]
+    return line
 
-print(lines)
-print(line)
+def calcPercent(userPhrase, correctPhrase):
+    return 100
+
+def calcWPM(chars, time):
+    return 100
+
+lineToType = getLine()
+startTime = time.time()
+userString = input(lineToType + "\n")
+endTime = time.time()
+
+print("TimeTaken: " + str(endTime - startTime) + " seconds")
+print("Percentage Correct:" + str(calcPercent("","")) + "%")
+print("WPM: " + str(calcWPM("","")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
